76edf3d73cf9bd03920fc7fa6c950b2054887328
fredabreu86 <fredabreu86@gmail.com>
2024-09-25 12:36:19 -0300

    Project created

---
+++ src/test/java/com/projetos/product_system/ProductSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.product_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
