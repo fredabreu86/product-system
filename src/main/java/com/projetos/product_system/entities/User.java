7e0f399bcdaf57ae936af4ec7753fff26512fd2e
fredabreu86 <fredabreu86@gmail.com>
2024-09-26 15:13:26 -0300

    Banco de dados H2, TEST profile e JPA

--- src/main/java/com/projetos/product_system/entities/User.java
+++ src/main/java/com/projetos/product_system/entities/User.java
@@ -1,15 +1,36 @@
 package com.projetos.product_system.entities;
 
 import java.io.Serializable;
+<<<<<<< HEAD
 import java.util.Objects;
 
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+=======
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name="tb_user")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+>>>>>>> 01287a7 (Banco de dados H2, TEST profile e JPA)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
+<<<<<<< HEAD
 	private String passord;
 	
 	public User() {
@@ -17,10 +38,20 @@ public class User implements Serializable{
 	}
 	public User(Long id, String name, String email, String phone, String passord) {
 		
+=======
+	private String password;
+
+	public User() {
+	}
+
+	public User(Long id, String name, String email, String phone, String password) {
+
+>>>>>>> 01287a7 (Banco de dados H2, TEST profile e JPA)
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
+<<<<<<< HEAD
 		this.passord = passord;
 	}
 	public Long getId() {
@@ -57,6 +88,59 @@ public class User implements Serializable{
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+=======
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+>>>>>>> 01287a7 (Banco de dados H2, TEST profile e JPA)
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -66,8 +150,18 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
+<<<<<<< HEAD
 		return Objects.equals(id, other.id);
 	}
 	
 	
+=======
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+>>>>>>> 01287a7 (Banco de dados H2, TEST profile e JPA)
 }
--- src/main/java/com/projetos/product_system/resources/UserResource.java
+++ src/main/java/com/projetos/product_system/resources/UserResource.java
@@ -10,10 +10,20 @@ import com.projetos.product_system.entities.User;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+<<<<<<< HEAD
 
 	@GetMapping
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Fred", "fredabreu86@gmail.com", "999999999", "123456");
 		return ResponseEntity.ok().body(u);
 	}
+=======
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Fred", "fredabreu86@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+>>>>>>> 01287a7 (Banco de dados H2, TEST profile e JPA)
 }
