c2bb2c8bcd9f0bc0ac5c197447d6b6991cbb4447
fredabreu86 <fredabreu86@gmail.com>
2024-09-27 09:56:54 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/projetos/product_system/resources/UserResource.java
+++ src/main/java/com/projetos/product_system/resources/UserResource.java
@@ -1,24 +1,36 @@
 package com.projetos.product_system.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetos.product_system.entities.User;
+import com.projetos.product_system.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Fred", "fredabreu86@gmail.com", "999999999", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 
-	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 	
 
 }
