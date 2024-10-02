f699619cae82ea501a3318ef81786458a160e055
fredabreu86 <fredabreu86@gmail.com>
2024-10-02 14:55:41 -0300

    Métodos subtotal e total de pagamento

--- src/main/java/com/projetos/product_system/entities/OrderItem.java
+++ src/main/java/com/projetos/product_system/entities/OrderItem.java
@@ -43,7 +43,6 @@ public class OrderItem implements Serializable {
 		id.setOrder(order);
 	}
 	
-	
 	public Product getProduct() {
 		return id.getProduct();
 	}
@@ -66,6 +65,10 @@ public class OrderItem implements Serializable {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+	
+	public Double getSubtotal() {
+		return price * quantity;
 	}
 	
 	@Override
